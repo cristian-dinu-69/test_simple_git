57cff8ce2e32f5f66630c86e6a1b3cc255c96940
cristian-dinu-69 <cristian69dinu@gmail.com>
2023-06-30 12:57:19 +0300

    class working

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
 class MathFunctions:
-    def add(a,b):
-        return a+b
-    def sub(a,b):
-        return a-b
-    def mul(a,b):
-        return a*b
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):
+        return self.a + self.b
+    
+
+    def sub(self):
+        return self.a - self.b
+    
+
+    def mul(self):
+        return self.a * self.b
+    
+
+test1 = MathFunctions(1,20)
+print(test1.a)
+print(test1.b)
+print(test1.add())
+print(test1.sub())
+print(test1.mul())
